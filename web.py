b2787a3a4fc7ef8db31f3f65a01706bc8fa55c2c
Niklas Lindblad <niklas@lindblad.info>
2017-07-01 12:01:57 +0100

    Restructure web app to account for Flask quirks

--- web.py
+++ web.py
@@ -11,40 +11,6 @@ from budget.config import Config
 from budget.database import *
 
 
-app = Flask(__name__, static_url_path='/static')
-google_login = GoogleLogin(app)
-login_manager = LoginManager(app)
-
-config = db = aggregator = None
-GOOGLE_LOGIN_CLIENT_ID = GOOGLE_LOGIN_CLIENT_SECRET = None
-
-
-def main():
-    global config, db, aggregator
-    global GOOGLE_LOGIN_CLIENT_ID, GOOGLE_LOGIN_CLIENT_SECRET
-
-    args = parse_arguments()
-
-    config = Config(args.config)
-    db = Database(config.get_database_uri())
-    aggregator = Aggregator(db)
-
-    if args.user_email:
-        config.pre_authenticated_user = args.user_email
-
-    GOOGLE_LOGIN_CLIENT_ID, GOOGLE_LOGIN_CLIENT_SECRET = config.get_google_auth_credentials()
-
-    app.config.update(
-        SECRET_KEY=GOOGLE_LOGIN_CLIENT_SECRET,
-        GOOGLE_LOGIN_REDIRECT_SCHEME="http",
-        GOOGLE_LOGIN_CLIENT_ID=GOOGLE_LOGIN_CLIENT_ID,
-        GOOGLE_LOGIN_CLIENT_SECRET=GOOGLE_LOGIN_CLIENT_SECRET
-    )
-
-    host = '0.0.0.0' if not args.debug else '127.0.0.1'
-    app.run(host=host, debug=args.debug)
-
-
 def parse_arguments():
     # Parse command line arguments
     args_parser = argparse.ArgumentParser()
@@ -62,8 +28,31 @@ def parse_arguments():
         "--user-email",
         help="Pre-authenticated user e-mail",
     )
-    args = args_parser.parse_args()
-    return args
+    return args_parser.parse_args()
+
+
+app = Flask(__name__, static_url_path='/static')
+
+args = parse_arguments()
+
+config = Config(args.config)
+db = Database(config.get_database_uri())
+aggregator = Aggregator(db)
+
+if args.user_email:
+    config.pre_authenticated_user = args.user_email
+
+google_client_id, google_client_secret = config.get_google_auth_credentials()
+
+app.config.update(
+    SECRET_KEY=google_client_secret,
+    GOOGLE_LOGIN_REDIRECT_SCHEME="https",
+    GOOGLE_LOGIN_CLIENT_ID=google_client_id,
+    GOOGLE_LOGIN_CLIENT_SECRET=google_client_secret
+)
+
+login_manager = LoginManager(app)
+google_login = GoogleLogin(app)
 
 
 def login_required(f):
@@ -117,6 +106,6 @@ def get_expenses_for_this_month():
     result = aggregator.get_expenses_for_this_month(person=g.user)
     return jsonify(result)
 
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    host = '0.0.0.0' if not args.debug else '127.0.0.1'
+    app.run(host=host, debug=args.debug)
