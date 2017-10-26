52f1bbe55c2652fa2e77c287c0c7642033e4327c
Niklas Lindblad <niklas@lindblad.info>
2017-10-26 21:39:21 +0100

    Improve Slack notification formatting

--- budget/slack.py
+++ budget/slack.py
@@ -12,17 +12,52 @@ class Slack(object):
     def notify(self, last_marker, person):
         currency = person.default_currency
         summary = self.aggregator.get_expenses_for_this_month(person)
-        sync_status = "successful :heavy_check_mark:" if last_marker.success else "failed :("
-        text = [
-            "*{}*".format(person.name),
-            "Last sync at {sync_time} {sync_status}".format(sync_time=last_marker.created_at, sync_status=sync_status)
+
+        status_color = "good" if last_marker.success else "danger"
+
+        fields = []
+        for group_name, group_spend in summary["total_sum_by_group"].iteritems():
+            fields.append({
+                "title": group_name,
+                "value": "{group_spend} {currency}".format(
+                    group_spend=group_spend,
+                    currency=currency
+                ),
+                "short": False
+            })
+        fields.append({
+            "title": "Total",
+            "value": "{total} {currency}".format(
+                total=summary["total_sum"],
+                currency=currency
+            ),
+            "short": False
+        })
+
+        print(fields)
+        print(summary["total_sum_by_group"])
+
+        attachments = [
+            {
+                "fallback": "Latest budget sync: {total} {currency}".format(
+                    total=summary["total_sum"],
+                    currency=currency
+                ),
+                "color": status_color,
+                "author_name": "Monthly spend",
+                "title": person.name,
+                "fields": fields,
+                "footer": "Budget",
+                "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+                "ts": last_marker.created_at.strftime('%s')
+            }
         ]
-        text += [" - {}: {} {}".format(k, v, currency) for k, v in summary["total_sum_by_group"].iteritems()]
-        text += ["*Total*: {total} {currency}".format(total=summary["total_sum"], currency=currency)]
+
         self.slack_client.chat.post_message(
             username=self.slack_config.bot_username,
-            as_user=False,
+            as_user=True,
             channel=self.slack_config.channel_name,
             icon_url=self.slack_config.icon_url,
-            text="\n".join(text)
+            attachments=attachments,
+            text=":money_with_wings: "
         )
